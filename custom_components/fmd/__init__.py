586492a15eb34a6c27b87a8f46d42ecfcb1244f6
Devin Slick <github@devinslick.com>
2025-10-14 22:15:56 -0500

    Fix entity creation

--- custom_components/fmd/__init__.py
+++ custom_components/fmd/__init__.py
@@ -5,9 +5,9 @@ from .const import DOMAIN
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up FMD from a config entry."""
-    hass.config_entries.async_setup_platforms(entry, ["device_tracker"])
+    await hass.config_entries.async_forward_entry_setups(entry, ["device_tracker"])
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    return await hass.config_entries.async_unload_platforms(entry, ["device_tracker"])
+    return await hass.config_entries.async_forward_entry_unload(entry, ["device_tracker"])
