1c06fd892d416d1845fb08914ec7fb6a998e850a
Devin Slick <github@devinslick.com>
2025-10-15 21:06:40 -0500

    Attempt to fix async blocking call

--- custom_components/fmd/__init__.py
+++ custom_components/fmd/__init__.py
@@ -1,13 +1,21 @@
 """The FMD integration."""
-from homeassistant.core import HomeAssistant
+from __future__ import annotations
+
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import Platform
+from homeassistant.core import HomeAssistant
+
 from .const import DOMAIN
+
+PLATFORMS: list[Platform] = [Platform.DEVICE_TRACKER]
+
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up FMD from a config entry."""
-    await hass.config_entries.async_forward_entry_setups(entry, ["device_tracker"])
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
+
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    return await hass.config_entries.async_forward_entry_unload(entry, ["device_tracker"])
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
